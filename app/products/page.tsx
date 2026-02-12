e814fc6efaae711ca2dde1e917885ac209602972
Yash Vataliya <yashvataliya65@gmail.com>
2026-02-12 18:51:56 +0530

    make products page force-dynamic

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,8 @@
 import ProductList from "@/app/components/ProductList";
 import { getProducts } from "../lib/products";
 
+export const dynamic = "force-dynamic";
+
 export default async function ProductsPage() {
   const products = await getProducts();
   return (
